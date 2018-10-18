18912a3f01cc685956ab6d515a08e5c910d45df9
William Seymour <william.seymour@kellogg.ox.ac.uk>
2018-10-18 16:28:25 +0100

    updated api and fixed a bug where resetting the db injected a bunch of amazon packets

--- api/api.py
+++ api/api.py
@@ -13,26 +13,31 @@ api = Api(app)
 log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
+REFRESH_INTERVAL = 60
+
+#Produce a summary of the main traffic destinations, and (in future) notify of any alerts that have been triggered
 class Digest(Resource):
     def get(self):
         refresh()
 
-        destinations = calculateImpacts()
-        topDest = ''
-        topImpact = 0
-        totalImpact = 0
-        for dest, impact in destinations.items():
-            totalImpact += impact
-            if impact > topImpact:
-                print(dest, impact)
-                topImpact = impact
+        impacts = calculateImpacts()
+        results = impacts['results']
+        total = impacts['total']
+        destinations = impacts['destinations']
+
+        topDest = {'name':'','impact':-1,'country':''}
+        for dest in destinations:
+            if dest['impact'] > topDest['impact']:
                 topDest = dest
 
-        digest = "Most traffic went to " + topDest + ' (' + str(int((topImpact/totalImpact)*100)) + '%)'
+        digest = "Tracking " + str(int(total/1000)) + " megabytes of traffic to " + str(results) + " different destinations."
+        digest += " The top destination was " + topDest['name'] + ' with ' + str(int((topDest['impact']/total)*100)) + '% of all traffic.'
+        digest += " No alerts have been triggered in the last 24 hours."
 
         print('Route /api/digest')
         return jsonify(digest)
 
+#Return a list of device names
 class Devices(Resource):
     def get(self):
         refresh()
@@ -40,6 +45,7 @@ class Devices(Resource):
         devices = jsonify(getDevices())
         return devices
 
+#Return a list of traffic destinations, along with which country they are in and how much data was sent there
 class Destinations(Resource):
     def get(self):
         refresh()
@@ -47,6 +53,7 @@ class Destinations(Resource):
         destinations = jsonify(calculateImpacts())
         return destinations
 
+#As for Destinations, but for a specific device name
 class DestinationsByDevice(Resource):
     def get(self, device):
         refresh()
@@ -54,15 +61,17 @@ class DestinationsByDevice(Resource):
         destinations = jsonify(calculateImpacts(device))
         return destinations
 
+#If iotData.json was last checked more than REFRESH_INTERVAL seconds ago, reload it now
 def refresh():
     global lastLoad
     global iotData
-    if (datetime.now() - lastLoad).total_seconds() > 60:
+    if (datetime.now() - lastLoad).total_seconds() > REFRESH_INTERVAL:
         print("Refreshing data")
         lastLoad = datetime.now()
         with open('../ui/src/assets/data/iotData.json') as raw:
             iotData = json.load(raw)
 
+#Return a list of devices in the iotData.json file
 def getDevices():
     refresh()
     global iotData
@@ -75,29 +84,50 @@ def getDevices():
 
     return devices
 
+#Return a list of impacts for the entire data set, or from a specific device
 def calculateImpacts(device=''):
     refresh()
     global iotData
 
     data = iotData['impacts']
-    impacts = {}
+    geos = iotData['geos']
+    returnObject = {}
+    destinations = []
+    results = 0
+    total = 0
 
     for impact in data:
-        if device == '' or device == impact['appid']:
-            if impact['companyName'] in impacts:
-                impacts[impact['companyName']] += impact['impact']
-            else:
-                impacts[impact['companyName']] = impact['impact']
+        found = False
+        for destination in destinations:
+            if device == '' or device == impact['appid']:
+                if destination['name'] == impact['companyName']:
+                    destination['impact'] += impact['impact']
+                    found = True
+                    break
+        if not found:
+            country = 'Unknown'
+            for geo in geos:
+                if geo['geo']['organisation'] == impact['companyName']:
+                    country = geo['geo']['country_name']
+            destinations.append({'name':impact['companyName'], 'impact':impact['impact'], 'country':country})
+            results += 1
+            total += impact['impact']
 
-    return impacts
+    returnObject['results'] = results
+    returnObject['total'] = total
+    returnObject['destinations'] = destinations
+    return returnObject
 
 if __name__ == '__main__':
+    #Register the API endpoints with flask
     api.add_resource(Digest, '/api/digest')
     api.add_resource(Devices, '/api/devices')
     api.add_resource(Destinations, '/api/destinations')
     api.add_resource(DestinationsByDevice, '/api/destinations/<device>')
 
+    #Initialise variables
     iotData = ""
     lastLoad = datetime.min
 
-    app.run(port=4201, threaded=True)
+    #Start the flask server
+    app.run(port=4201, threaded=True, host='0.0.0.0')
--- capture/resetDb.py
+++ capture/resetDb.py
@@ -21,38 +21,38 @@ with open(os.path.join(DATAPATH,"iotData.json"), 'w') as fp:
 
 DB_MANAGER.execute(open(os.path.join(os.path.dirname(FILE_PATH), "db", "schema.sql"), "rb").read(), "")
 
-first = True
-
-for packet in rdpcap(os.path.join(os.path.join(os.path.dirname(FILE_PATH),"tests"), "testData", "AlexaTime1")):
-    if first:
-        #first = False
-        try:
-            sql = """INSERT INTO packets(time, src, dst, mac, len, proto) VALUES(%s, %s, %s, %s, %s, %s) RETURNING id;"""
-
-            t = datetime.datetime.utcfromtimestamp(packet.time)
-            strTime = t.strftime('%d/%m/%Y %H:%M:%S.%f')
-            scr = packet[IP].src
-            dst = packet[IP].dst
-            if "192.168" in packet[IP].src:
-                mac = packet.src
-            else:
-                mac = packet.dst
-                
-
-            length = str( int(packet.len) + 14 )
-
-            proto = packet[TCP].dport
-
-            DB_MANAGER.execute(sql, (strTime, scr, dst, mac, length, proto), all=False)
-
-        except IndexError:
-            pass
-
-getAll = """ SELECT * FROM packets ORDER BY id"""
-result = DB_MANAGER.execute(getAll, "")
-
-for row in result:
-    print(row)
-
-packetBurstification()
-burstPrediction()
+#first = True
+#
+#for packet in rdpcap(os.path.join(os.path.join(os.path.dirname(FILE_PATH),"tests"), "testData", "AlexaTime1")):
+#    if first:
+#        #first = False
+#        try:
+#            sql = """INSERT INTO packets(time, src, dst, mac, len, proto) VALUES(%s, %s, %s, %s, %s, %s) RETURNING id;"""
+#
+#            t = datetime.datetime.utcfromtimestamp(packet.time)
+#            strTime = t.strftime('%d/%m/%Y %H:%M:%S.%f')
+#            scr = packet[IP].src
+#            dst = packet[IP].dst
+#            if "192.168" in packet[IP].src:
+#                mac = packet.src
+#            else:
+#                mac = packet.dst
+#                
+#
+#            length = str( int(packet.len) + 14 )
+#
+#            proto = packet[TCP].dport
+#
+#            DB_MANAGER.execute(sql, (strTime, scr, dst, mac, length, proto), all=False)
+#
+#        except IndexError:
+#            pass
+#
+#getAll = """ SELECT * FROM packets ORDER BY id"""
+#result = DB_MANAGER.execute(getAll, "")
+#
+#for row in result:
+#    print(row)
+#
+#packetBurstification()
+#burstPrediction()
