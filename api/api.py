2a04a43c293a63d27c3445c4fb334de345b20348
mcnutty <william.seymour@kellogg.ox.ac.uk>
2018-11-14 12:24:23 +0000

    almost finished new api impacts

--- api/api.py
+++ api/api.py
@@ -14,6 +14,10 @@ DB_MANAGER = databaseBursts.dbManager()
 
 app = Flask(__name__)
 api = Api(app)
+CACHE_TIMEOUT = 10
+ID_POINTER = 0
+impacts = []
+geos = dict()
 
 #=============
 #api endpoints
@@ -21,7 +25,7 @@ api = Api(app)
 #return aggregated data for the given time period (called by refine)
 class Refine(Resource):
     def get(self, days):
-        return jsonify({"bursts": GetBursts(days), "macMan": MacMan(), "manDev": ManDev()})
+        return jsonify({"bursts": GetBursts(days), "macMan": MacMan(), "manDev": ManDev(), "impacts": GetImpacts(days), "usage": GenerateUsage()})
 
 #get the mac addresse, manufacturer, and custom name of every device
 class Devices(Resource):
@@ -57,13 +61,76 @@ def ManDev():
 
 #get geo data for an ip
 def GetGeo(ip):
-    _,lat,lon,c_code = DB_MANAGER.execute("SELECT * FROM geodata WHERE ip=%s LIMIT 1", (ip,), False)
-    geo = {"latitude": lat, "longitude": lon, "country_code": c_code}
+    lat,lon,c_code,c_name = DB_MANAGER.execute("SELECT lat, lon, c_code, c_name FROM geodata WHERE ip=%s LIMIT 1", (ip,), False)
+    geo = {"latitude": lat, "longitude": lon, "country_code": c_code, "company_name": c_name}
     return geo
 
 #get bursts for the given time period in days
 def GetBursts(days):
-    pass
+    bursts = DB_MANAGER.execute("SELECT MIN(time), MIN(mac), burst, MIN(categories.name) FROM packets JOIN bursts ON bursts.id = packets.burst JOIN categories ON categories.id = bursts.category WHERE time > (NOW() - INTERVAL %s) GROUP BY burst ORDER BY burst", ("'" + str(days) + " DAY'",))
+    
+    result = []
+    #epoch = datetime.datetime.utcfromtimestamp(0)
+    epoch = datetime(1970, 1, 1, 0, 0)
+    for burst in bursts:
+        unixTime = int((burst[0] - epoch).total_seconds() * 1000.0)
+        device = burst[1]
+        category = burst[3]
+        result.append({"value": unixTime, "category": category, "device": device })
+    return result
+
+def GetImpacts(days):
+    global geos, ID_POINTER
+    packets = DB_MANAGER.execute("SELECT * FROM packets WHERE id > %s AND time > (NOW() - INTERVAL %s) ORDER BY id", (str(ID_POINTER), "'" + str(days) + " DAY'"))
+    result = []
+    local_ip_mask = re.compile('^(192\.168|10\.|255\.255\.255\.255).*')
+
+    #[0] id
+    #[1] timestamp
+    #[2] src
+    #[3] dst
+    #[4] mac
+    #[5] len
+    #[6] proto
+    #[7] burst
+
+    for packet in packets:
+        #determine if the src or dst is the external ip address
+        ip_src = local_ip_mask.match(packet[2]) is not None
+        ip_dst = local_ip_mask.match(packet[3]) is not None
+        ext_ip = None
+        if (ip_src and ip_dst) or (not ip_src and not ip_dst):
+            continue #shouldn't happen, either 0 or 2 internal hosts
+        elif ip_src:
+            ext_ip = packet[3]
+        else:
+            ext_ip = packet[2]
+        #make sure we have geo data, then update the impact
+        if ext_ip not in geos:
+            geos[ext_ip] = GetGeo(ext_ip)
+        UpdateImpact(packet[4], ext_ip)
+        if ID_POINTER < packet[0]:
+            ID_POINTER = packet[0]
+
+    #build a list of all device/ip impacts and geo data
+    for ip,geo in geos.items():
+        for mac,_ in ManDev().items():
+            item = geo
+            item['impact'] = GetImpact(mac, ip)
+            item['ip'] = ip
+            item['device'] = mac
+            result.append(item)
+    return result
+
+def UpdateImpact(mac, ip):
+    global impacts
+
+def GetImpact(mac, ip):
+    global impacts
+    return 4
+
+def GenerateUsage():
+    return []
 
 if __name__ == '__main__':
     #Register the API endpoints with flask
