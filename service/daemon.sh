af43abc5da69cb6bb552b9f342d124901bf4ebcc
mcnutty <william.seymour@kellogg.ox.ac.uk>
2018-12-03 15:54:08 +0000

    fixed path bug in daemon

--- service/daemon.sh
+++ service/daemon.sh
@@ -19,36 +19,28 @@ trap 'term' QUIT
 
 #move to working directory
 cd /home/mcnutty/Documents/IoT-refine
+cd scripts
 
 #start capturing packets
-cd capture
 ./capture.py &
 PID3=$!
 >&2 echo Started capture with PID $PID3
+
+#start categorising packets
+sudo -u mcnutty ./loop.py &
+PID4=$!
+>&2 echo Started categorisation with PID $PID4
+
+#start the API
+sudo -u mcnutty ./api.py &
+PID5=$!
+>&2 echo Started the API with PID $PID5
 
 #start angular
 cd ../ui
 sudo -u mcnutty ng serve &
 PID1=$!
 >&2 echo Started angular with PID $PID1
-
-#start crunchbase
-#cd ../crunchbaseSupport
-#sudo -u mcnutty node google-serv.js &
-#PID2=$!
-#>&2 echo Started crunchbase with PID $PID2
-
-#start categorising packets
-cd ../categorisation
-sudo -u mcnutty ./loop.py &
-PID4=$!
->&2 echo Started categorisation with PID $PID4
-
-#start the API
-cd ../api
-sudo -u mcnutty ./api.py &
-PID5=$!
->&2 echo Started the API with PID $PID5
 
 #sleep until sent a SIGINT or SIGTERM
 while [ 1 ]; do
