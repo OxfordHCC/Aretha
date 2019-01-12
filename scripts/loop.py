6d83d369d56409ca27db2da63c35e2e561968313
electronic Max <max@hip.cat>
2019-01-12 16:45:08 +0000

    happy little caching of raw_ips

--- scripts/loop.py
+++ scripts/loop.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import sys, time, os, signal, requests, re, argparse
+import sys, time, os, signal, requests, re, argparse, json
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "db"))
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "categorisation"))
 import databaseBursts
@@ -11,6 +11,8 @@ DB_MANAGER = databaseBursts.dbManager()
 INTERVAL = 1
 LOCAL_IP_MASK = re.compile('^(192\.168|10\.|255\.255\.255\.255).*') #so we can filter for local ip addresses
 DEBUG = False
+RAW_IPS = None
+_events = [] # async db events
 
 config = {"EchoFlowNumberCutoff":10,"burstNumberCutoffs":{"Echo":20,"Google Home":60,"Philips Hue Bridge":2,"Unknown":10},"burstTimeIntervals":{"Echo":1,"Google Home":1,"Philips Hue Bridge":1,"Unknown":1}}
 
@@ -116,7 +118,12 @@ def burstPrediction(devices):
 
 def processGeos():
     # raw_ips = DB_MANAGER.execute("SELECT DISTINCT src, dst FROM packets", ())
-    raw_ips = set( [r[0] for r in DB_MANAGER.execute("SELECT DISTINCT src FROM packets", ())]).union([r[0] for r in DB_MANAGER.execute("SELECT DISTINCT dst FROM packets", ())])
+    global RAW_IPS
+
+    if not RAW_IPS:
+        print("Preloading RAW_IPS")
+        RAW_IPS = set( [r[0] for r in DB_MANAGER.execute("SELECT DISTINCT src FROM packets", ())]).union([r[0] for r in DB_MANAGER.execute("SELECT DISTINCT dst FROM packets", ())])
+        print(" Done ", len(RAW_IPS), " known ips ")
         
     # print("raw_ips", raw_ips)
     
@@ -126,7 +133,7 @@ def processGeos():
     for row in raw_geos:
         known_ips.append(row[0])
 
-    for ip in raw_ips:
+    for ip in RAW_IPS:
         if LOCAL_IP_MASK.match(ip) is not None:
             # local ip, so skip
             continue
@@ -151,6 +158,22 @@ def processMacs():
                 DB_MANAGER.execute("INSERT INTO devices VALUES(%s, %s, 'unknown')", (mac[0], manufacturer[:20]))
             else:
                 DB_MANAGER.execute("INSERT INTO devices VALUES(%s, 'unknown', 'unknown')", (mac[0],))
+
+
+#
+
+def processEvents():
+    global _events
+    cur_events = _events.copy()
+    _events.clear()
+    for evt in cur_events:
+        evt = json.loads(evt)
+        if RAW_IPS and evt["operation"] in ['UPDATE','INSERT'] and evt["table"] == 'packets':
+            # print("adding to raw ips %s %s" % (evt["data"]["src"],evt["data"]["dst"]))
+            RAW_IPS.add(evt["data"]["src"])
+            RAW_IPS.add(evt["data"]["dst"])
+        pass
+
 
 #============
 #loop control
@@ -179,8 +202,12 @@ if __name__ == '__main__':
     #register the signal handler
     handler = sigTermHandler() 
 
+    # watch for listen events -- not sure if this has to be on its own connection
+    DB_MANAGER.listen('db_notifications', lambda payload:_events.append(payload))
+
     #loop through categorisation tasks
     while(True):
+        processEvents()
         processGeos()
         processMacs()
         devices = requests.get(url='http://localhost:4201/api/devices').json()["manDev"]
