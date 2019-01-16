c79bd56563cd1c678a08640b26360a8a68caa673
mcnutty <william.seymour@kellogg.ox.ac.uk>
2019-01-16 15:23:09 +0000

    linux config bugfixes

--- scripts/reset-browser.sh
+++ scripts/reset-browser.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 killall chromium #stop currently open browser window(s)
-chromium --start-fullscreen --app=http://localhost:4200 http://localhost:4200 & #open chromium fullscreen on iotrefine
+nohup chromium --start-fullscreen --app=http://localhost:4200 http://localhost:4200 & #open chromium fullscreen on iotrefine
--- scripts/soft-reset.sh
+++ scripts/soft-reset.sh
@@ -3,10 +3,13 @@
 LOCATION=/home/mcnutty/Documents/IoT-refine/scripts/
 
 cd $LOCATION
+echo Service down...
 service iotrefine stop
 sleep 1
 ./reset-database.py
 sleep 1
+echo Service up...
 service iotrefine start
+echo Waiting for everything to start...
 sleep 20
-./reset-browser.sh
+env DISPLAY=0.0 ./reset-browser.sh
