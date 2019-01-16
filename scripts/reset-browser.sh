45b7a0b2ad66edcb6ce365b53aace5f18dbf1d0b
mcnutty <william.seymour@kellogg.ox.ac.uk>
2019-01-16 16:14:44 +0000

    more bugz squashed

--- scripts/reset-browser.sh
+++ scripts/reset-browser.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 killall chromium #stop currently open browser window(s)
-nohup chromium --start-fullscreen --app=http://localhost:4200 http://localhost:4200 & #open chromium fullscreen on iotrefine
+/usr/bin/chromium --start-fullscreen --app=http://localhost:4200 http://localhost:4200 & #open chromium fullscreen on iotrefine
--- scripts/reset-wifi.sh
+++ scripts/reset-wifi.sh
@@ -4,5 +4,5 @@ nmcli r wifi off #turn wifi off
 sleep 1
 nmcli r wifi on #turn wifi on
 sleep 1
-nmcli d wifi hotspot ifname wlp58s0 ssid "IoT Refine Demo" password "heresmydata" #start hotspot on built-in wifi card
+nmcli d wifi hotspot ifname wlp58s0 ssid "IoT Refine Demo" password "wheresmydata" #start hotspot on built-in wifi card
 sleep 1
