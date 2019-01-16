347db7946ac382f021ae0ae8ab3a6fc922547da7
mcnutty <william.seymour@kellogg.ox.ac.uk>
2019-01-16 17:18:40 +0000

    made login script wipe database

--- scripts/login-delay.sh
+++ scripts/login-delay.sh
@@ -1,6 +1,12 @@
 #! /bin/sh
 
-echo Starting IoT-refine
+LOCATION=/home/mcnutty/Documents/IoT-refine/scripts/
+
+echo Starting IoT-refine\n
+echo Clearing database...
+cd $LOCATION
+./reset-database.sh
+
 echo Waiting for service to come up...
 sleep 15
 chromium --start-fullscreen --app=http://localhost:4200 http://localhost:4200
