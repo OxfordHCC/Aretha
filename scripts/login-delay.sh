482df5f4bfdfe4a32f83266484bf565ea53e493a
mcnutty <william.seymour@kellogg.ox.ac.uk>
2019-01-16 17:41:36 +0000

    wording tweak

--- scripts/login-delay.sh
+++ scripts/login-delay.sh
@@ -2,11 +2,8 @@
 
 LOCATION=/home/mcnutty/Documents/IoT-refine/scripts/
 
-echo Starting IoT-refine\n
-echo Clearing database...
 cd $LOCATION
 ./reset-database.py
-
 echo Waiting for service to come up...
 sleep 15
 chromium --start-fullscreen --app=http://localhost:4200 http://localhost:4200
