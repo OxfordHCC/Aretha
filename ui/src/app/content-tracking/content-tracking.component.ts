2bebfb5ee4b79216a41c2a42834e81e98d2b24e4
mcnutty <william.seymour@kellogg.ox.ac.uk>
2019-06-27 16:53:44 +0100

    made some of the content better + further reading

--- ui/src/app/content-tracking/content-tracking.component.ts
+++ ui/src/app/content-tracking/content-tracking.component.ts
@@ -11,7 +11,7 @@ import {Router} from '@angular/router';
 export class ContentTrackingComponent implements OnInit {
 
 	@Input() stage: number;
-	max: number = 2 + 1; //+1 for the attention check at the end
+	max: number = 4 + 1; //+1 for the attention check at the end
 	done: boolean = false;
 	answerText: string;
 	tipText: string;
