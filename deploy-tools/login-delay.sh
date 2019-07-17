7b86351d59e65c0aaddaf49acb2192993a746d71
mcnutty <william.seymour@kellogg.ox.ac.uk>
2019-07-17 14:35:22 +0100

    deploy script tweak

--- deploy-tools/login-delay.sh
+++ deploy-tools/login-delay.sh
@@ -1,9 +1,16 @@
 #! /bin/sh
 
-LOCATION=/home/mcnutty/Documents/IoT-refine/scripts/
+# Create a .desktop file in ~/.config/autostart
+#
+#[Desktop Entry]
+#Name=IoTRefine
+#GenericName=IoTRefine
+#Comment=Refine for the smart home
+#Exec=/path/to/this/script
+#Terminal=false
+#Type=Application
+#X-GNOME-Autostart-enabled=true
 
-cd $LOCATION
-./reset-database.py
-echo Waiting for service to come up...
-sleep 15
+echo Starting IoT-Refine, please be patient :D
+sleep 30
 chromium --start-fullscreen --app=http://localhost:4200 http://localhost:4200
