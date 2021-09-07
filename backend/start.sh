9d275293d048fd48a28123ce421acf7b723f4cb0
treebirg <treebirg@gmail.com>
2021-09-07 14:04:54 +0300

    deployment ops

--- backend/start.sh
+++ backend/start.sh
@@ -1,22 +1,27 @@
 #! /bin/bash
 
+# exit when any command fails
+set -e
+
 #start the wifi hotspot
 #./reset-wifi.sh
 
 #start capturing packets
 python -m capture &
-PID1=$!
->&2 echo Started capture with PID $PID1
+PID_CAPTURE=$!
 
 #start categorising packets
 python -m loop &
-PID2=$!
->&2 echo Started categorisation with PID $PID2
+PID_LOOP=$!
 
 #start the API
 python -m api &
-PID3=$!
->&2 echo Started the API with PID $PID3
+PID_API=$!
+
+
+wait $PID_CAPTURE
+wait $PID_LOOP
+wait $PID_API
 
 #sleep until sent a SIGINT or SIGTERM
 while [ 1 ]; do
